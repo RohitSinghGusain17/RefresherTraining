53c89ae0267814c84ec26c86dad2bd28226853d5
Rohit Singh Gusain <rohitgusain672432@gmail.com>
2026-08-01 20:24:51 +0530

    Day5 work

---
+++ Day5/Topbrains/StringsArea/StringsArea/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IArea
+{
+    double GetArea();
+}
+
+public abstract class Shape : IArea
+{
+    public abstract double GetArea();
+}
+
+public class Circle : Shape
+{
+    private double r;
+
+    public Circle(double radius)
+    {
+        r = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * r * r;
+    }
+}
+
+public class Rectangle : Shape
+{
+    private double w, h;
+
+    public Rectangle(double width, double height)
+    {
+        w = width;
+        h = height;
+    }
+
+    public override double GetArea()
+    {
+        return w * h;
+    }
+}
+
+public class Triangle : Shape
+{
+    private double b, h;
+
+    public Triangle(double baseVal, double height)
+    {
+        b = baseVal;
+        h = height;
+    }
+
+    public override double GetArea()
+    {
+        return 0.5 * b * h;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        string[] shapes = {
+            "C 3",
+            "R 4 5",
+            "T 6 2"
+        };
+
+        double totalArea = ComputeTotalArea(shapes);
+        Console.WriteLine("Total Area = " + totalArea);
+    }
+
+    public static double ComputeTotalArea(string[] shapes)
+    {
+        List<Shape> shapeList = new List<Shape>();
+
+        foreach (var s in shapes)
+        {
+            string[] parts = s.Split(' ');
+            string type = parts[0];
+
+            if (type == "C")
+            {
+                double r = double.Parse(parts[1]);
+                shapeList.Add(new Circle(r));
+            }
+            else if (type == "R")
+            {
+                double w = double.Parse(parts[1]);
+                double h = double.Parse(parts[2]);
+                shapeList.Add(new Rectangle(w, h));
+            }
+            else if (type == "T")
+            {
+                double b = double.Parse(parts[1]);
+                double h = double.Parse(parts[2]);
+                shapeList.Add(new Triangle(b, h));
+            }
+        }
+
+        double total = 0;
+        foreach (var shape in shapeList)
+        {
+            total += shape.GetArea();
+        }
+
+        return Math.Round(total, 2, MidpointRounding.AwayFromZero);
+    }
+}
